8eaee6777a37119040923c47d5f21f965e11e457
Lutfi Syahreza Lubis <lutfisyahrezalubis@apps.ipb.ac.id>
2026-06-04 16:09:44 +0800

    quiz

--- test/Quiz/QuizView.swift
+++ test/Quiz/QuizView.swift
@@ -5,11 +5,30 @@
 //  Created by Lutfi Syahreza Lubis on 04/06/26.
 //
 
+import Lottie
 import SwiftUI
 
 struct QuizView: View {
+    @State private var phase: AnimationPhase = .idle
+    @State private var currentStep: QuizStep = .finished
 
-    @State private var currentStep: QuizStep = .quiz1
+    enum AnimationPhase {
+        case idle, forward, reverse
+    }
+
+    var playbackMode: LottiePlaybackMode {
+        switch phase {
+        case .idle: return .paused(at: .progress(0))
+        case .forward:
+            return .playing(
+                .fromProgress(0, toProgress: 1, loopMode: .playOnce)
+            )
+        case .reverse:
+            return .playing(
+                .fromProgress(1, toProgress: 0, loopMode: .playOnce)
+            )
+        }
+    }
 
     var body: some View {
 
@@ -31,15 +50,15 @@ struct QuizView: View {
 
             ZStack {
                 Color(
-                    red: 141/255,
-                    green: 193/255,
-                    blue: 246/255
+                    red: 141 / 255,
+                    green: 193 / 255,
+                    blue: 246 / 255
                 )
                 .ignoresSafeArea()
 
                 VStack {
-                    Text("🎉")
-                        .font(.system(size: 80))
+//                    Text("🎉")
+//                        .font(.system(size: 80))
 
                     Text("Congratulations!")
                         .font(
@@ -49,12 +68,38 @@ struct QuizView: View {
                             )
                         )
                         .foregroundStyle(.white)
+                    LottieView {
+                        try await DotLottieFile.named("sparkles")
+                    }
+                    .playbackMode(playbackMode)
+                    .animationDidFinish { completed in
+                        guard completed else { return }
+                        switch phase {
+                        case .forward:
+                            DispatchQueue.main.asyncAfter(
+                                deadline: .now() + 0.05
+                            ) {
+                                phase = .reverse
+                            }
+                        case .reverse:
+                            phase = .idle
+                        case .idle:
+                            break
+                        }
+                    }
+                    .onAppear{
+                        phase = .forward
+                    }
+//                    .onTapGesture {
+//                        guard phase == .idle else { return }
+//                        phase = .forward
+//                    }
                 }
             }
         }
     }
 }
 
-//#Preview {
-//    QuizView(currentStep: .quiz1)
-//}
+#Preview {
+    QuizView()
+}
